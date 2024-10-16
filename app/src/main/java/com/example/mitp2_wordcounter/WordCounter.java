d165177a7767fd28d435f9780ac44d10fad535f2
S4ytto <saytto@posteo.com>
2024-10-16 19:27:01 +0300

    Final

--- app/src/main/java/com/example/mitp2_wordcounter/WordCounter.java
+++ app/src/main/java/com/example/mitp2_wordcounter/WordCounter.java
@@ -1,2 +1,22 @@
-package com.example.mitp2_wordcounter;public class WordCounter {
+package com.example.mitp2_wordcounter;
+
+public class WordCounter {
+
+    public static int getWordsCount(String text) {
+        String[] words = text.trim().split("\\s+|,|\\.|;|!|\\?");
+        return words.length;
+    }
+
+    public static int getPunctuationCount(String text) {
+        int punctuationCount = 0;
+
+        String punctuation = ".,;:!?()[]{}'\"-=`~|_/><@#$%^&*";
+
+        for (char c : text.toCharArray()) {
+            if (punctuation.indexOf(c) >= 0) {
+                punctuationCount++;
+            }
+        }
+        return punctuationCount;
+    }
 }
